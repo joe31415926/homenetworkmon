7447dffc355abf9f4df9abac2b457de4990a7280
Joseph Ruff <joeruff@yahoo.com>
2022-01-08 10:24:17 -0800

    come on: keep alive

--- cronautossh.sh
+++ cronautossh.sh
@@ -5,5 +5,5 @@ done
 
 while :
 do
-/usr/bin/ssh -N -R 9100:localhost:22 -o StrictHostKeyChecking=no -o ServerAliveInterval=15 -i /home/pi/.ssh/weaksecurity ubuntu@`cat /home/pi/ramdisk/ip`
+/usr/bin/ssh -N -R 9100:localhost:22 -o StrictHostKeyChecking=no -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -i /home/pi/.ssh/weaksecurity ubuntu@`cat /home/pi/ramdisk/ip`
 done
