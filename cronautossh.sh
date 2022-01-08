3f909960776416aee093510a921a4f4ec8faa2f8
Joseph Ruff <joeruff@yahoo.com>
2022-01-08 10:27:57 -0800

    a shot in the dark

--- cronautossh.sh
+++ cronautossh.sh
@@ -6,4 +6,5 @@ done
 while :
 do
 /usr/bin/ssh -N -R 9100:localhost:22 -o StrictHostKeyChecking=no -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -i /home/pi/.ssh/weaksecurity ubuntu@`cat /home/pi/ramdisk/ip`
+sleep 5
 done
