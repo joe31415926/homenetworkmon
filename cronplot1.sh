351ebae533175f8e5b509e03e5a7031609b1cbbe
Joseph Ruff <joeruff@yahoo.com>
2022-01-08 18:51:50 -0800

    typo

--- cronplot1.sh
+++ cronplot1.sh
@@ -2,5 +2,5 @@
 while :
 do inotifywait -e delete_self /home/pi/ramdisk/log1.txt
    python3 /home/pi/homenetworkmon/plot.py < /home/pi/ramdisk/log1.txt > /home/pi/ramdisk/log1.tmp.rgba
-   mv /home/pi/ramdisk/log1.tmp.png /home/pi/ramdisk/log1.rgba
+   mv /home/pi/ramdisk/log1.tmp.rgba /home/pi/ramdisk/log1.rgba
 done
--- cronplot3.sh
+++ cronplot3.sh
@@ -2,5 +2,5 @@
 while :
 do inotifywait -e delete_self /home/pi/ramdisk/log3.txt
    python3 /home/pi/homenetworkmon/plot.py < /home/pi/ramdisk/log3.txt > /home/pi/ramdisk/log3.tmp.rgba
-   mv /home/pi/ramdisk/log3.tmp.png /home/pi/ramdisk/log3.rgba
+   mv /home/pi/ramdisk/log3.tmp.rgba /home/pi/ramdisk/log3.rgba
 done
