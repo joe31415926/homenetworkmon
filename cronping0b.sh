33e5a0f1b02745f3bbb93a9a0218518ad162de58
Joseph Ruff <joeruff@yahoo.com>
2022-02-26 06:38:54 -0800

    zero wait

--- cronping0b.sh
+++ cronping0b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-echo 0 | nc -u 127.0.0.1 8080
+echo 0 | nc -w 0 -u 127.0.0.1 8080
 ping -c 1 -w 10 -p 0 -I wlan0 `cat /home/pi/configure/pingip0.txt`
 sleep 5
 done
--- cronping3b.sh
+++ cronping3b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-echo 3 | nc -u 127.0.0.1 8080
+echo 3 | nc -w 0 -u 127.0.0.1 8080
 ping -c 1 -w 10 -p 3 -I wlan0 `cat /home/pi/ramdisk/ip`
 sleep 5
 done
